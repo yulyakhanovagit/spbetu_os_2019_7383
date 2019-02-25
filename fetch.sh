c990bcfaca008be87888ef0e11809abfc8b593db
JAkutenshi <jakutenshi@gmail.com>
2019-02-25 16:23:05 +0300

    add current ready students to fetch script

--- fetch.sh
+++ fetch.sh
@@ -1,19 +1,19 @@
-# git fetch fanboyalliance
-# git fetch marlen99
+git fetch fanboyalliance
+git fetch marlen99
 # git fetch ArtemVasilev
 # git fetch ravlasov
-# git fetch danilzuev21
+git fetch danilzuev21
 # git fetch welarie
-# git fetch Falvram
-# git fetch FineGun
+git fetch Falvram
+git fetch FineGun
 git fetch laskovenko1
 git fetch DmLvkvch
 git fetch LosevMikhail
 # git fetch AngelinaMarkova99
-# git fetch vanokako
-# git fetch ProkopenkoNadezhda
+git fetch vanokako
+git fetch ProkopenkoNadezhda
 # git fetch Dyuha
 # git fetch Lucky324
-# git fetch Katty57
+git fetch Katty57
 # git fetch yulyakhanovagit
 # git fetch Kairiss
